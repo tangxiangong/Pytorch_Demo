3de8847b30127b44899813789fed97d1e662cf39
tangx <tangxg16@gmail.com>
2020-07-22 11:40:25 +0800

    init

--- chapter12/pokemon.py
+++ chapter12/pokemon.py
@@ -16,21 +16,33 @@ class Pokemon(Dataset):
             if not os.path.isdir(os.path.join(self.root, name)):
                 continue
             self.name2label[name] = len(self.name2label.keys())
-        print(self.name2label)
+        self.images, self.labels = self.load_csv('pokemon.csv')
 
     def load_csv(self, filename: str):
-        images = []
-        for name in self.name2label.keys():
-            images += glob.glob(os.path.join(self.root, name, '*.png'))
-            images += glob.glob(os.path.join(self.root, name, '*.jpg'))
-            images += glob.glob(os.path.join(self.root, name, '*.jpeg'))
-        random.shuffle(images)
-        with open(os.path.join(self.root, filename), mode='w', newline='') as f:
-            writer = csv.writer(f)
-            for img in images:
-                name = img.split(os.sep)[-2]
-                label = self.name2label[name]
-                writer.writerow([img, label])
+        if not os.path.exists(os.path.join(self.root, filename)):
+            images = []
+            for name in self.name2label.keys():
+                images += glob.glob(os.path.join(self.root, name, '*.png'))
+                images += glob.glob(os.path.join(self.root, name, '*.jpg'))
+                images += glob.glob(os.path.join(self.root, name, '*.jpeg'))
+            random.shuffle(images)
+            with open(os.path.join(self.root, filename), mode='w', newline='') as f:
+                writer = csv.writer(f)
+                for img in images:
+                    name = img.split(os.sep)[-2]
+                    label = self.name2label[name]
+                    writer.writerow([img, label])
+        images_read, labels_read = [], []
+        with open(os.path.join(self.root, filename), mode='r') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                image_read, label_read = row
+                label_read = int(label_read)
+                images_read.append(image_read)
+                labels_read.append(label_read)
+
+        assert len(images_read) == len(labels_read)
+        return images_read, labels_read
 
     def __len__(self):
         pass
